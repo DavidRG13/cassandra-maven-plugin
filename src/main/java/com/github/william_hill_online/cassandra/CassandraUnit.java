04fb63410550ac64a714bd3e746087033be82a76
David Rodriguez <david.rodriguez@williamhill.com>
2017-06-06 08:09:44 +0200

    Change package name

--- src/main/java/com/github/william_hill_online/cassandra/CassandraUnit.java
+++ src/main/java/com/github/william_hill_online/cassandra/CassandraUnit.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.williamhill.cassandra;
+package com.github.william_hill_online.cassandra;
 
 import org.apache.commons.io.FileUtils;
 
